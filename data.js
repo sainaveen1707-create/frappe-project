e8c0ff61813577aeec0998a2ea072dc378ba3fe7
SAINAVEEN M <sainaveen1707@gmail.com>
2026-04-07 12:52:54 +0530

    alternative calculation program data.js
    Removed commented-out msgprint statements for clarity.

---
+++ data.js
@@ -0,0 +1,137 @@
+frappe.ui.form.on('test data', {
+
+
+    refresh: function(frm) {
+        calculate_all(frm);
+    },
+
+    
+    product_1: function(frm) {
+        calculate_all(frm);
+    },
+
+   
+    product_2: function(frm) {
+        calculate_all(frm);
+    },
+
+  
+    operator: function(frm) {
+        calculate_all(frm);
+    },
+
+   
+    discount: function(frm) {
+        calculate_all(frm);
+    },
+
+    
+    tax: function(frm) {
+        calculate_all(frm);
+    },
+
+   
+    discount_product_1: function(frm) {
+        apply_discount(frm, 'product_1');
+    },
+
+   
+    discount_product_2: function(frm) {
+        apply_discount(frm, 'product_2');
+    },
+
+    
+    discount_total: function(frm) {
+        apply_discount(frm, 'total');
+    },
+
+   
+    discount_tax: function(frm) {
+        apply_discount(frm, 'tax');
+    }
+
+});
+
+
+function calculate_all(frm) {
+    let p1       = flt(frm.doc.product_1);
+    let p2       = flt(frm.doc.product_2);
+    let operator = frm.doc.operator || '+';
+    let discount = flt(frm.doc.discount);
+    let tax_pct  = flt(frm.doc.tax);
+
+   
+    let total = 0;
+    if (operator === '+') total = p1 + p2;
+    else if (operator === '-') total = p1 - p2;
+    else if (operator === '*') total = p1 * p2;
+    else if (operator === '/') total = p2 !== 0 ? p1 / p2 : 0;
+
+    frappe.model.set_value(frm.doctype, frm.docname, 'total', flt(total, 2));
+
+   
+    let tax_amount = flt((total * tax_pct) / 100, 2);
+
+
+    let grand_total = flt(total + tax_amount, 2);
+
+    frappe.model.set_value(frm.doctype, frm.docname, 'grand_total', grand_total);
+}
+
+
+function apply_discount(frm, target) {
+    let p1       = flt(frm.doc.product_1);
+    let p2       = flt(frm.doc.product_2);
+    let operator = frm.doc.operator || '+';
+    let discount = flt(frm.doc.discount);
+    let tax_pct  = flt(frm.doc.tax);
+
+    
+    let total = 0;
+    if (operator === '+') total = p1 + p2;
+    else if (operator === '-') total = p1 - p2;
+    else if (operator === '*') total = p1 * p2;
+    else if (operator === '/') total = p2 !== 0 ? p1 / p2 : 0;
+
+    let disc_amt   = 0;
+    let base_total = total;
+    let tax_amount = 0;
+    let grand_total = 0;
+
+    if (target === 'product_1') {
+        
+        disc_amt   = flt((p1 * discount) / 100, 2);
+        base_total = flt(total - disc_amt, 2);
+        tax_amount = flt((base_total * tax_pct) / 100, 2);
+        grand_total = flt(base_total + tax_amount, 2);
+     
+
+    } else if (target === 'product_2') {
+        
+        disc_amt   = flt((p2 * discount) / 100, 2);
+        base_total = flt(total - disc_amt, 2);
+        tax_amount = flt((base_total * tax_pct) / 100, 2);
+        grand_total = flt(base_total + tax_amount, 2);
+       
+
+    } else if (target === 'total') {
+       
+        disc_amt   = flt((total * discount) / 100, 2);
+        base_total = flt(total - disc_amt, 2);
+        tax_amount = flt((base_total * tax_pct) / 100, 2);
+        grand_total = flt(base_total + tax_amount, 2);
+        
+
+    } else if (target === 'tax') {
+        
+        let original_tax = flt((total * tax_pct) / 100, 2);
+        disc_amt   = flt((original_tax * discount) / 100, 2);
+        tax_amount = flt(original_tax - disc_amt, 2);
+        grand_total = flt(total + tax_amount, 2);
+       
+    }
+
+    
+    frappe.model.set_value(frm.doctype, frm.docname, 'grand_total', grand_total);
+    frm.refresh_fields();
+}
